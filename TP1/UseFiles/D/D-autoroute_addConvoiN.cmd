8529bb44152b0a8bb66f23936b0e2504eafa046f
Janelle Brousseau <philippe.janelle-brousseau.1@ens.etsmtl.ca>
2018-02-13 16:25:43 -0500

    init proofs for operations

---
+++ TP1/UseFiles/D/D-autoroute_addConvoiN.cmd
@@ -0,0 +1,21 @@
+!create Manageur : Manageur
+
+!create Conducteur1 : Conducteur
+!create Camera1 : Camera
+
+!create A1 : Autoroute
+!set A1.Nom := 'A1'
+!set A1.Numero := 20
+!insert (Manageur, A1) into ManageurAutoroute
+
+!create Vehicule1 : Vehicule
+!set Vehicule1.Direction := 'A'
+!insert (Vehicule1, Conducteur1) into vehiculeConducteur
+!insert (A1, Vehicule1) into AutorouteVehicule
+
+!create Vehicule2 : Vehicule
+!set Vehicule2.Direction := 'B'
+!insert (Vehicule2, Camera1) into vehiculeCamera
+!insert (A1, Vehicule2) into AutorouteVehicule
+
+!A1.addConvoi(Set{Vehicule1, Vehicule2}, Vehicule1)
--- TP1/request.md
+++ TP1/request.md
@@ -2,6 +2,13 @@
 
 # Requêtes
 
-Présentez ici chaque requête séparément. 
-Débutez par expliquer ce que réalise la 
-requête puis présentez sa simulation.
+## ListeConvoisActifs:
+
+##### Explication
+
+Cet requêtes retourne tout les convois actif. Un convoi est actil si il contient au moins un vehicule.
+Notre architecture defini un convoi comme un convoi necessairement actif pusque tout convoi dois nesesairement avoir un vehicule.
+
+```
+    listeConvoisActifs() : Set(Convoi) = self.autoroutes.convois->flatten()->select(c | c.vehicules->size() > 1)->asSet()
+```
