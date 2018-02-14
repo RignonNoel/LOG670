03a2b2ece2c7900ca09c75cde336da73b6528d68
PhilippeJanelle <philippe.janelle-brousseau.1@ens.etsmtl.ca>
2018-02-13 22:06:26 -0500

    Finish prof opperation

---
+++ TP1/UseFiles/D/D-autoroute_fusionConvoiN.cmd
@@ -0,0 +1,46 @@
+!create Manageur : Manageur
+
+!create Conducteur1 : Conducteur
+!create Conducteur2 : Conducteur
+!create Sonar1 : Sonar
+!create Sonar2 : Sonar
+
+!create A1 : Autoroute
+!set A1.Nom := 'A1'
+!set A1.Numero := 20
+!insert (Manageur, A1) into ManageurAutoroute
+
+!create A2 : Autoroute
+!set A2.Nom := 'A2'
+!set A2.Numero := 50
+!insert (Manageur, A2) into ManageurAutoroute
+
+!create Convoi1 : Convoi
+!insert (A1, Convoi1) into AutorouteConvoi
+
+!create Convoi2 : Convoi
+!insert (A2, Convoi1) into AutorouteConvoi
+
+!create Vehicule1 : Vehicule
+!set Vehicule1.Direction := 'A'
+!insert (Vehicule1,Convoi1) into convoiLeader
+!insert (Convoi1, Vehicule1) into convoiVehicules
+!insert (Vehicule1, Conducteur1) into vehiculeConducteur
+
+!create Vehicule3 : Vehicule
+!set Vehicule3.Direction := 'A'
+!insert (Convoi1, Vehicule3) into convoiVehicules
+!insert (Vehicule3, Conducteur2) into vehiculeConducteur
+
+!create Vehicule4 : Vehicule
+!set Vehicule4.Direction := 'A'
+!insert (Vehicule4,Convoi2) into convoiLeader
+!insert (Convoi2, Vehicule4) into convoiVehicules
+!insert (Vehicule4, Sonar1) into vehiculeSonar
+
+!create Vehicule5 : Vehicule
+!set Vehicule5.Direction := 'A'
+!insert (Convoi2, Vehicule5) into convoiVehicules
+!insert (Vehicule5, Sonar2) into vehiculeSonar
+
+!A1.fusionConvoi(Convoi1, Convoi2)
---
+++ TP1/UseFiles/D/D-convoi_removeVehiculeN.cmd
@@ -0,0 +1,29 @@
+!create Manageur : Manageur
+
+!create Conducteur1 : Conducteur
+!create Conducteur2 : Conducteur
+
+!create A1 : Autoroute
+!set A1.Nom := 'A1'
+!set A1.Numero := 20
+!insert (Manageur, A1) into ManageurAutoroute
+
+!create Convoi1 : Convoi
+!insert (A1, Convoi1) into AutorouteConvoi
+
+!create Convoi2 : Convoi
+!insert (A1, Convoi1) into AutorouteConvoi
+
+!create Vehicule1 : Vehicule
+!set Vehicule1.Direction := 'A'
+!insert (Vehicule1,Convoi1) into convoiLeader
+!insert (Convoi1, Vehicule1) into convoiVehicules
+!insert (Vehicule1, Conducteur1) into vehiculeConducteur
+
+!create Vehicule2 : Vehicule
+!set Vehicule2.Direction := 'A'
+!insert (Vehicule2, Convoi2) into convoiLeader
+!insert (Convoi2, Vehicule2) into convoiVehicules
+!insert (Vehicule2, Conducteur2) into vehiculeConducteur
+
+!Convoi1.removeVehicule(Vehicule2)
